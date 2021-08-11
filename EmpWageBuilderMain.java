e281f1de92e9326f5ccb127fb817c6494b34a73c
dipalimagare <kalpeshmahajan90@gmail.com>
2021-08-11 20:37:16 +0530

    Uploading EmpWageBuilder File

---
+++ EmpWageBuilderMain.java
@@ -0,0 +1,15 @@
+package com.bl;
+
+public class EmpWageBuilderMain {
+
+	public static void main(String[] args) {
+		
+		EmpWageBuilder.Attendance();
+		EmpWageBuilder.getDailyWager();
+		EmpWageBuilder.getMonthlyWageWhileCondition();
+		EmpWageBuilder.getMonthlyWage();
+		EmpWageBuilder.getPartTimeWage();
+		EmpWageBuilder.usingSwitch();
+		
+	}
+}
